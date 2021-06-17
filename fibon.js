7f3a436ceeee31144e49f888cfcdf63407ac6ea6
Anson-To <55016933+Anson-To@users.noreply.github.com>
2021-06-17 22:27:50 +0800

    Add fibon.js, tested with chrome console

---
+++ fibon.js
@@ -0,0 +1,30 @@
+function fib(n) 
+{
+    if (n <= 1)
+        return n;
+    return fib(n-1) + fib(n-2);
+}
+
+function closeFibon(input)
+{
+//max fibon number that is smaller than input
+var n = 0;
+while(fib(n)<input)
+{n++;}
+return fib(n-1);
+}
+
+
+function formerFibonacci(array)
+{
+var result=new Array();
+for(var i =0;i<array.length;i++)
+{
+//closeFibon(array[i])
+console.log(closeFibon(array[i]));
+result.push(closeFibon(array[i]));
+}
+//console.log(result);
+}
+
+formerFibonacci([1,5,9,22]);
